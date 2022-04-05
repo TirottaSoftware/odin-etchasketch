1a09bbbc12c23e60a76f0283ced663660d712dfd
TirottaSoftware <cristiantirotta@gmail.com>
2022-04-05 09:25:45 +0300

    Implemented grid size input logic

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const container = document.querySelector('.main-container')
+const gridSizeInput = document.querySelector('#input-grid-size');
+const applyButton = document.querySelector('.btn-apply');
 
-let gridSize = 16;
+applyButton.addEventListener('click', () => {
+    loadGrid(gridSizeInput.value)
+})
 
-for (let j = 0; j < gridSize; j++) {
-    for (let i = 0; i < gridSize; i++) {
-        let flexElement = document.createElement('div');
-        flexElement.classList.add('flex-element');
-        flexElement.style.width = `calc(100% / ${gridSize})`
-        flexElement.style.height = `calc(100% / ${gridSize})`
-            flexElement.addEventListener('mouseenter', () => {
-                flexElement.classList.toggle('color');
-            })
-        container.appendChild(flexElement)
+function loadGrid(gridSize){
+    container.innerHTML = '';
+    
+    for (let j = 0; j < gridSize; j++) {
+        for (let i = 0; i < gridSize; i++) {
+            let flexElement = document.createElement('div');
+            flexElement.classList.add('flex-element');
+            flexElement.style.width = `calc(100% / ${gridSize})`
+            flexElement.style.height = `calc(100% / ${gridSize})`
+                flexElement.addEventListener('mouseenter', () => {
+                    flexElement.classList.toggle('color');
+                })
+            container.appendChild(flexElement)
+        }
     }
 }
+
+loadGrid(16)
